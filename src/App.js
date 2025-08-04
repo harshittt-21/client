9c2bdf73412433a494d87607f1ce14688f224bfe
harshittt-21 <harshitrawat2102@gmail.com>
2025-08-04 21:35:40 +0530

    k

--- src/App.js
+++ src/App.js
@@ -109,7 +109,7 @@ const AppLayout = () => {
       
       <footer className="footer">
         <div className="container">
-          <p>&copy; 2024 ShopNetic. All rights reserved.</p>
+          <p>&copy; 2025 ShopNetic. All rights reserved.</p>
         </div>
       </footer>
     </div>
